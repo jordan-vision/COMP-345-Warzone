29eaaaffc989c1c429d70988dc9d4db46007ec94
allyjenna <allyjenna@me.com>
2023-02-12 16:44:34 -0500

    hopefully final push for player

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,6 @@
 #include "Player.h"
 #include "Orders.h"
+#include "Cards.h"
 #include <iostream>
 #include <vector>
 #include <string>
@@ -18,6 +19,7 @@ Player::~Player() {
     for (Territory* ta : territoriesAttack) 
         delete ta; 
     delete myOrders;
+    delete myHand;
 };
 
 //Constructor that initializes the player's collection of territories, hand of cards, and the list of orders
@@ -64,7 +66,7 @@ vector<Territory*> Player::toDefend() {
     return territoriesDefend;
 
 }
-//Implementation of the toAttack() method
+//Implementation of the toAttack() method 
 vector<Territory*> Player::toAttack() {
     vector<Territory*> territoriesAttack;
     territoriesAttack.push_back(new Territory("Mexico"));
@@ -85,96 +87,52 @@ vector<Territory*> Player::toAttack() {
 
 void Player::issueOrder(string orderType)
 { 
-
-   // check the orderType and print the appropriate order
-   
+   // checking the orderType and then printing the order
    if (orderType == "Deploy") {
-       // create orders 
-       Deploy* d = new Deploy();
+      // create order 
+      Deploy* d = new Deploy();
       cout<< "\n" <<orderType << " order has been issued:\n" << endl;
       cout << *d << endl;
       //add to order list
       ol->add(d);
    } else if (orderType == "Advance") {
-    Advance* a = new Advance();
+     // create order 
+     Advance* a = new Advance();
      cout<< orderType << " order has been issued:\n" << endl;
       cout << *a << endl;
+      //add to order list
       ol->add(a);
    } else if (orderType == "Blockade") {
-     Blockade* bl = new Blockade();
+      // create order 
+      Blockade* bl = new Blockade();
       cout<< orderType << " order has been issued:\n" << endl;
       cout << *bl << endl;
+      //add to order list
       ol->add(bl);
    } else if (orderType == "Bomb") {
-       Bomb* bo = new Bomb();
+      // create order 
+      Bomb* bo = new Bomb();
       cout<< orderType << " order has been issued:\n" << endl;
       cout << *bo << endl;
+      //add to order list
       ol->add(bo);
    } else if (orderType == "Airlift") {
-       Airlift* ar = new Airlift();
+      // create order 
+      Airlift* ar = new Airlift();
       cout<< orderType << " order has been issued:\n" << endl;
-    cout << *ar << endl;
-    ol->add(ar);
+      cout << *ar << endl;
+      //add to order list
+      ol->add(ar);
    } else if (orderType == "Negotiate") {
-       Negotiate* n = new Negotiate();
+      // create order 
+      Negotiate* n = new Negotiate();
       cout<< orderType << " order has been issued:\n" << endl;
       cout << *n << endl;
+      //add to order list
       ol->add(n);
    } else {
       cout << "Invalid order type" << endl;
    }
     cout << "\nOrders List: " << endl;
-   cout << *ol << endl;}
-
-// //Implementation of the issueOrder() method
-// void Player::issueOrder(string orderType)
-// { 
-//    // create an OrderList object
-//    OrdersList* ol = new OrdersList();
-
-
-//    // create orders of all types
-//    Deploy* d = new Deploy();
-//    Advance* a = new Advance();
-//    Blockade* bl = new Blockade();
-//    Bomb* bo = new Bomb();
-//    Airlift* ar = new Airlift();
-//    Negotiate* n = new Negotiate();
-
-   
-//    cout << *ol << endl;
-
-    // if (orderType == "Deploy")
-    // {
-    //     myOrders->add(new Deploy());
-     
-        
-    // }
-    // else if (orderType == "Advance")
-    // {
-    //     myOrders->add(new Advance());
-    // }
-    // else if (orderType == "Bomb")
-    // {
-    //     myOrders->add(new Bomb());
-    // }
-    // else if (orderType == "Blockade")
-    // {
-    //     myOrders->add(new Blockade());
-    // }
-    // else if (orderType == "Airlift")
-    // {
-    //     myOrders->add(new Airlift());
-    // }
-    // else if (orderType == "Negotiate")
-    // {
-    //     myOrders->add(new Negotiate());
-    // }
-    // else
-    // {
-    //     cout << "Invalid order type!" << endl;
-    // }
-    
-//         cout << "\nIssued orders: " << endl;
-
-// }
+    cout << *ol << endl;
+}
