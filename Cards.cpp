010c7d87244de68dffe72307dcaf313d3ae51761
Parker HB <95991855+ParkerHB@users.noreply.github.com>
2023-02-12 14:07:28 -0500

    Fixed string issue

--- Cards.cpp
+++ Cards.cpp
@@ -134,10 +134,10 @@ ostream& operator<<(std::ostream& out, const Card* card) {
 void Card::play(Player* player, Deck* mainDeck) {
 
     // Adds order to player orders according to string value of cardtype
-    if (CardTypeString[static_cast<int>(*(this->cardType))] == "Diplomacy") {
+    if (strcmp(CardTypeString[static_cast<int>(*(this->cardType))], "Diplomacy") == 0) {
         player->issueOrder("Negotiate");
     }
-    else if (CardTypeString[static_cast<int>(*(this->cardType))] == "Reinforcement") 
+    else if (strcmp(CardTypeString[static_cast<int>(*(this->cardType))], "Reinforcement") == 0) 
         player->issueOrder("Advance");
     else 
         player->issueOrder(CardTypeString[static_cast<int>(*(this->cardType))]);
