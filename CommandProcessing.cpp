3a276d3ad00585ff4e199b55db9172fc445f687e
Koosa924 <afaf.anzarouti@gmail.com>
2023-03-18 19:21:49 -0400

    warning fix

--- CommandProcessing.cpp
+++ CommandProcessing.cpp
@@ -206,7 +206,7 @@ ostream& operator <<(ostream& output, FileCommandProcessorAdapter& adapter){
     return output<<"\nThis is a file command processor adapter object"<<endl;
 }
 
-Command* FileCommandProcessorAdapter:: readCommand(string fileName){
+void FileCommandProcessorAdapter:: readCommand(string fileName){
     Command* command = flr->readLineFromFile(fileName);
 
     // after validate method has been implemented, call it here on the command returned from above to check it 
