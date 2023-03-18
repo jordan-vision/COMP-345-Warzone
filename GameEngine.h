cea0b4432affab0a3242ab21e456e06167a767cf
VikaTheDuck <victoriusolo@gmail.com>
2023-03-18 12:36:14 -0400

    Revert "uncomment logger"
    This reverts commit 526670ae35e9e5b1c6c33c594e7fb14ac3f3bb6a.

--- GameEngine.h
+++ GameEngine.h
@@ -1,6 +1,6 @@
 
 #pragma once
-#include "LoggingObserver.h"
+// #include "LoggingObserver.h"
 #include <string>
 #include <vector>
 #include <iostream>
@@ -81,7 +81,8 @@ public:
 ostream& operator<<(ostream& out, Transition& transition);
 
 
-class GameEngine : public ILoggable, public Subject 
+class GameEngine 
+// : public ILoggable, public Subject 
 {
 private:
 	static bool isGameOver;
@@ -101,7 +102,7 @@ public:
 
 	bool transition(string command);
 
-	virtual string stringToLog() override;
+	// virtual string stringToLog() override;
 
 	//startup phase method implements a command-based user interaction mechanism 
 	//to start the game by allowing the user to proceed with the game startup phase:
