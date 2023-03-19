a97169bc89086581d99fa57e9ff246d058ce0125
Parker HB <95991855+ParkerHB@users.noreply.github.com>
2023-03-19 12:30:53 -0400

    Updated LoggingObserver

--- LoggingObserver.h
+++ LoggingObserver.h
@@ -7,6 +7,9 @@ class ILoggable {
 
     public:
         ~ILoggable();
+        //ILoggable(const ILoggable &ilog);                               // Copy Constructor
+        //ILoggable& operator =(const ILoggable &ilog);                   // Assignment Operator
+        //friend ostream& operator <<(ostream& out, const ILoggable* ilog); // Stream Operator
         virtual string stringToLog() =0;
 };
 
@@ -14,6 +17,8 @@ class Observer {
     
     public:
         ~Observer();
+        Observer& operator =(const Observer &observer);                   // Assignment Operator
+        friend ostream& operator <<(ostream& out, const Observer* observer); // Stream Operator
         virtual void Update(ILoggable* log) =0;
     protected:
         Observer();
@@ -24,6 +29,9 @@ class LogObserver : public Observer {
     public:
         LogObserver();
         ~LogObserver();
+        LogObserver(const LogObserver &logobserver);                               // Copy Constructor
+        LogObserver& operator =(const LogObserver &logobserver);                   // Assignment Operator
+        friend ostream& operator <<(ostream& out, const LogObserver* logobserver); // Stream Operator
         virtual void Update(ILoggable* log) override;
 };
 
@@ -35,6 +43,9 @@ class Subject {
         virtual void Notify(ILoggable* log);
         Subject();
         ~Subject();
+        Subject(const Subject &subject);                                   // Copy Constructor
+        Subject& operator =(const Subject &subject);                       // Assignment Operator
+        friend ostream& operator <<(ostream& out, const Subject* subject); // Stream Operator
     private:
         list<Observer*> *_observers;
 };
--- LoggingObserverDriver.cpp
+++ LoggingObserverDriver.cpp
@@ -6,34 +6,48 @@ using namespace std;
 
 int main() {
 	
+    Player player;
     Command* myCommand = new Command();
     CommandProcessor* myCommandProcessor = new CommandProcessor();
     OrdersList* myOrders = new OrdersList();
     GameEngine* myEngine = new GameEngine();
 
-    Deploy* d = new Deploy();
-    Advance* a = new Advance();
-    Blockade* bl = new Blockade();
-    Bomb* bo = new Bomb();
-    Airlift* ar = new Airlift();
-    Negotiate* n = new Negotiate();
+    MapLoader* loader = new MapLoader();
+    Map* map5 = loader->loadMap("europe.map");      
+    map5->validate();
+
+    // Prints territories of a continent in map 5 - europe.map
+    for(auto continent: map5->getContinents()){
+        cout<<"\nContinent "<<*continent<<" has territories: "<<endl;
+        for(auto territory: continent->getTerritories()){
+            cout<<*territory<<endl;
+        }
+    }
+
+
+    // Deploy* d = new Deploy();
+    // Advance* a = new Advance();
+    // Blockade* bl = new Blockade();
+    // Bomb* bo = new Bomb();
+    // Airlift* ar = new Airlift();
+    // Negotiate* n = new Negotiate();
 
    
-    myCommand->saveEffect();
-    myCommandProcessor->saveCommand();
-    myOrders->add(d);
-    myOrders->add(a);
-    myOrders->add(bl);
-    myOrders->add(bo);
-    myOrders->add(ar);
-    myOrders->add(n);
+    // myCommand->saveEffect();
+    // myCommandProcessor->saveCommand();
+    // myOrders->add(d);
+    // myOrders->add(a);
+    // myOrders->add(bl);
+    // myOrders->add(bo);
+    // myOrders->add(ar);
+    // myOrders->add(n);
     
-    d->execute();
-    a->execute();
-    bl->execute();
-    bo->execute();
-    ar->execute();
-    n->execute();
+    // d->execute();
+    // a->execute();
+    // bl->execute();
+    // bo->execute();
+    // ar->execute();
+    // n->execute();
 
     while (!myEngine->getIsGameOver()) {
 		string input;
